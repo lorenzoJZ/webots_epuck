45b62c3ae5a118264dbf0782d512dbe33555ea3e
lorenzoJZ <92121714+lorenzoJZ@users.noreply.github.com>
2021-10-10 14:31:31 -0300

    Alterações do Trabalho

--- motoristas/epuck_map/epuck_map.c
+++ motoristas/epuck_map/epuck_map.c
@@ -1,166 +1,166 @@
-# inclua  < webots / robot.h >
-# inclui  < webots / motor.h >
-# inclui  < webots / distance_sensor.h >
-# include  < webots / position_sensor.h >
-# inclua  < webots / supervisor.h >
+#include <webots/robot.h>
+#include <webots/motor.h>
+#include <webots/distance_sensor.h>
+#include <webots/position_sensor.h>
+#include <webots/supervisor.h>
 
-# inclui  < stdio.h >
-# inclui  < stdlib.h >
-# inclui  < string.h >
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-# define  TIME_STEP  64
-# define  MAX_SPEED  6.28
+#define TIME_STEP 64
+#define MAX_SPEED 6.28
 
-# define  WHEEL_DISTANCE  0,052     // Distância entre rodas em metros (comprimento do eixo); é o mesmo valor de "WHEEL_SEPARATION", mas expresso em metros.
-# define  WHEEL_RADIUS  0,0205     // m.
-# define  WHEEL_CIRCUMFERENCE WHEEL_RADIUS * M_PI * 2.0     // Circunferência da roda (metros).
-# define  ROBOT_RADIUS  0,035  // metros.
-# define  RANGE_MIN  0,005 + ROBOT_RADIUS // 0,5 cm + ROBOT_RADIUS.
-# define  RANGE_MAX  0,05 + ROBOT_RADIUS // 5 cm + ROBOT_RADIUS.
+#define WHEEL_DISTANCE 0.052    // Distance between wheels in meters (axis length); it's the same value as "WHEEL_SEPARATION" but expressed in meters.
+#define WHEEL_RADIUS 0.0205    // m.
+#define WHEEL_CIRCUMFERENCE WHEEL_RADIUS*M_PI*2.0    // Wheel circumference (meters).
+#define ROBOT_RADIUS 0.035 // meters.
+#define RANGE_MIN 0.005+ROBOT_RADIUS // 0.5 cm + ROBOT_RADIUS.
+#define RANGE_MAX 0.05+ROBOT_RADIUS // 5 cm + ROBOT_RADIUS. 
 
-// posições de proximidade do e-puck (cm), x apontando para frente, y apontando para a esquerda
-//            P7 (3,5, 1,0) P0 (3,5, -1,0)
-//        P6 (2,5, 2,5) P1 (2,5, -2,5)
-//    P5 (0,0, 3,0) P2 (0,0, -3,0)
-//        P4 (-3,5, 2,0) P3 (-3,5, -2,0)
+// e-puck proximity positions (cm), x pointing forward, y pointing left
+//           P7(3.5, 1.0)   P0(3.5, -1.0)
+//       P6(2.5, 2.5)           P1(2.5, -2.5)
+//   P5(0.0, 3.0)                   P2(0.0, -3.0)
+//       P4(-3.5, 2.0)          P3(-3.5, -2.0)
 
-// orentações de proximidade do e-puck (graus)
-//            P7 (10) P0 (350)
-//        P6 (40) P1 (320)
-//    P5 (90) P2 (270)
-//        P4 (160) P3 (200)
+// e-puck proximity orentations (degrees)
+//           P7(10)   P0(350)
+//       P6(40)           P1(320)
+//   P5(90)                   P2(270)
+//       P4(160)          P3(200)
 
-typedef  struct {
+typedef struct {
 
 WbDeviceTag right_motor;  
 WbDeviceTag left_motor;
 WbFieldRef robot_position;
 WbFieldRef robot_rotation;
-WbDeviceTag ps [ 8 ];
+WbDeviceTag ps[8];
 
-} Robot_concepts;
+}Robot_concepts;
 
-Robot_concepts robot_init () {
+Robot_concepts robot_init() {
   
-  Conceitos de conceitos de robô;
-  wb_robot_init ();
-  conceitos. left_motor = wb_robot_get_device ( " motor da roda esquerda " );
-  conceitos. motor_direito = wb_robot_get_device ( " motor da roda direita " );
-  wb_motor_set_position (conceitos. left_motor , INFINITY);
-  wb_motor_set_position (conceitos. right_motor , INFINITY);
-  wb_motor_set_velocity (conceitos. left_motor , 0,1 * MAX_SPEED);
-  wb_motor_set_velocity (conceitos. right_motor , 0,1 * MAX_SPEED);
-  WbDeviceTag left_encoder = wb_robot_get_device ( " sensor da roda esquerda " );
-  WbDeviceTag right_encoder = wb_robot_get_device ( " sensor da roda direita " );
-  wb_position_sensor_enable (left_encoder, TIME_STEP);
-  wb_position_sensor_enable (right_encoder, TIME_STEP);
-    char ps_id [ 4 ];
-    para ( int i = 0 ; i < 8 ; i ++) {
-      sprintf (ps_id, " ps % d " , i);
-      conceitos. ps [i] = wb_robot_get_device (ps_id);
-      wb_distance_sensor_enable (conceitos. ps [i], TIME_STEP);
+  Robot_concepts concepts;
+  wb_robot_init();
+  concepts.left_motor = wb_robot_get_device("left wheel motor");
+  concepts.right_motor = wb_robot_get_device("right wheel motor");
+  wb_motor_set_position(concepts.left_motor, INFINITY);
+  wb_motor_set_position(concepts.right_motor, INFINITY);
+  wb_motor_set_velocity(concepts.left_motor, 0.1 * MAX_SPEED);
+  wb_motor_set_velocity(concepts.right_motor, 0.1 * MAX_SPEED);
+  WbDeviceTag left_encoder = wb_robot_get_device("left wheel sensor");
+  WbDeviceTag right_encoder = wb_robot_get_device("right wheel sensor");
+  wb_position_sensor_enable(left_encoder, TIME_STEP);
+  wb_position_sensor_enable(right_encoder, TIME_STEP);
+    char ps_id[4];
+    for(int i = 0; i < 8; i++){
+      sprintf(ps_id, "ps%d", i);
+      concepts.ps[i] = wb_robot_get_device(ps_id);
+      wb_distance_sensor_enable(concepts.ps[i], TIME_STEP);
     }
-  WbNodeRef robot_node = wb_supervisor_node_get_from_def ( " EPUCK " );
-    if (robot_node == NULL ) {
-    fprintf (stderr, " Nenhum nó DEF EPUCK encontrado no arquivo mundial atual \ n " );
-    saída ( 1 );
+  WbNodeRef robot_node = wb_supervisor_node_get_from_def("EPUCK");
+    if (robot_node == NULL) {
+    fprintf(stderr, "No DEF EPUCK node found in the current world file\n");
+    exit(1);
     }
-  conceitos. robot_position = wb_supervisor_node_get_field (robot_node, " tradução " );
-  conceitos. robot_rotation = wb_supervisor_node_get_field (robot_node, " rotação " );
+  concepts.robot_position = wb_supervisor_node_get_field(robot_node, "translation");
+  concepts.robot_rotation = wb_supervisor_node_get_field(robot_node, "rotation");
 
-  conceitos de retorno ;
+  return concepts;
 }
   
-bool  detect_obstacle_ahead ( float d [ 8 ]) {
-  return ((d [ 0 ] <RANGE_MAX) ||
-           (d [ 1 ] <RANGE_MAX) ||
-           (d [ 6 ] <RANGE_MAX) ||
-           (d [ 7 ] <RANGE_MAX));
+bool detect_obstacle_ahead(float d[8]) {
+  return ( (d[0] < RANGE_MAX) || 
+           (d[1] < RANGE_MAX) || 
+           (d[6] < RANGE_MAX) || 
+           (d[7] < RANGE_MAX));
 }
 
-float  convert_intensity_to_meters ( float prox) {
-  float dist = 0,0 ;
-  if (prox> 0,0 )
-    dist = 0,5 / sqrt (prox) + ROBOT_RADIUS;
-  outro
+float convert_intensity_to_meters(float prox) {
+  float dist = 0.0;
+  if (prox > 0.0)
+    dist = 0.5/sqrt(prox)+ROBOT_RADIUS;
+  else
     dist = RANGE_MAX;
-  if (dist> RANGE_MAX)
+  if (dist > RANGE_MAX)
     dist = RANGE_MAX;
   return dist;
 }
  
-void  salvar_posicao_distancias (FILE * log, float x, float y, float angulo, float distancia [ 8 ]) {
-    fprintf ( log , " % f  % f  % f  " , x, y, angulo);
-    para ( int i = 0 ; i < 8 ; i ++) {
-      fprintf ( log , " % f  " , distancia [i]);
+void salvar_posicao_distancias(FILE *log, float x, float y, float angulo, float distancia[8]){
+    fprintf(log, "%f %f %f ", x, y, angulo);
+    for(int i = 0; i < 8; i++) {
+      fprintf(log, "%f ", distancia[i]);
     }
-    fprintf ( log , " \ n " );
-    fflush ( log );
+    fprintf(log, "\n");
+    fflush(log);
 }
 
-int  main ( int argc, char ** argv) {
-  ARQUIVO * log = fopen ( " log.csv " , " w " );
-  if (! log )
-    saída ( 1 );
+int main(int argc, char **argv) {
+  FILE *log = fopen("log.csv", "w");
+  if (!log)
+    exit(1);
     
-   // // EXERCICIO: CRIAR FUNÇÃO PARA SUBSTITUIR ESSE CODIGO USANDO STRUCT /////
+   //// EXERCICIO: CRIAR FUNÇÃO PARA SUBSTITUIR ESSE CODIGO USANDO STRUCT /////
   
-   // wb_robot_init ();
-   // WbDeviceTag left_motor = wb_robot_get_device ("motor da roda esquerda");
-   // WbDeviceTag right_motor = wb_robot_get_device ("motor da roda direita");
-   // wb_motor_set_position (left_motor, INFINITY);
-   // wb_motor_set_position (right_motor, INFINITY);
-   // wb_motor_set_velocity (left_motor, 0,1 * MAX_SPEED);
-   // wb_motor_set_velocity (right_motor, 0,1 * MAX_SPEED);
-   // WbDeviceTag left_encoder = wb_robot_get_device ("sensor da roda esquerda");
-   // WbDeviceTag right_encoder = wb_robot_get_device ("sensor da roda direita");
-   // wb_position_sensor_enable (left_encoder, TIME_STEP);
-   // wb_position_sensor_enable (right_encoder, TIME_STEP);
-   // WbDeviceTag ps [8];
-   // char ps_id [4];
-   // para (int i = 0; i <8; i ++) {
-   // sprintf (ps_id, "ps% d", i);
-   // ps [i] = wb_robot_get_device (ps_id);
-   // wb_distance_sensor_enable (ps [i], TIME_STEP);
+   // wb_robot_init();
+   // WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
+   // WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
+   // wb_motor_set_position(left_motor, INFINITY);
+   // wb_motor_set_position(right_motor, INFINITY);
+   // wb_motor_set_velocity(left_motor, 0.1 * MAX_SPEED);
+   // wb_motor_set_velocity(right_motor, 0.1 * MAX_SPEED);
+   // WbDeviceTag left_encoder = wb_robot_get_device("left wheel sensor");
+   // WbDeviceTag right_encoder = wb_robot_get_device("right wheel sensor");
+   // wb_position_sensor_enable(left_encoder, TIME_STEP);
+   // wb_position_sensor_enable(right_encoder, TIME_STEP);
+   // WbDeviceTag ps[8];
+   // char ps_id[4];
+   // for(int i = 0; i < 8; i++){
+   // sprintf(ps_id, "ps%d", i);
+   // ps[i] = wb_robot_get_device(ps_id);
+   // wb_distance_sensor_enable(ps[i], TIME_STEP);
    // }
-   // WbNodeRef robot_node = wb_supervisor_node_get_from_def ("EPUCK");
+   // WbNodeRef robot_node = wb_supervisor_node_get_from_def("EPUCK");
    // if (robot_node == NULL) {
-   // fprintf (stderr, "Nenhum nó DEF EPUCK encontrado no arquivo mundial atual \ n");
-   // exit (1);
+   // fprintf(stderr, "No DEF EPUCK node found in the current world file\n");
+   // exit(1);
    // }
-   // WbFieldRef robot_position = wb_supervisor_node_get_field (robot_node, "tradução");
-   // WbFieldRef robot_rotation = wb_supervisor_node_get_field (robot_node, "rotação");
+   // WbFieldRef robot_position = wb_supervisor_node_get_field(robot_node, "translation");
+   // WbFieldRef robot_rotation = wb_supervisor_node_get_field(robot_node, "rotation");
   
-   // // EXERCICIO: CRIAR FUNÇÃO PARA SUBSTITUIR ESSE CODIGO USANDO STRUCT /////
-  Robot_concepts settings = robot_init ();
-  float dist [ 8 ];
-  float x = 0 , y = 0 , teta = 0 ;
-  float left_steps_prev = 0 , right_steps_prev = 0 ;
-  enquanto ( wb_robot_step (TIME_STEP)! = - 1 ) {
-    const  double * position = wb_supervisor_field_get_sf_vec3f (configurações. robot_position );
-    const  duplo * rotação = wb_supervisor_field_get_sf_rotation (configurações. robot_rotation );
+   //// EXERCICIO: CRIAR FUNÇÃO PARA SUBSTITUIR ESSE CODIGO USANDO STRUCT /////
+  Robot_concepts settings = robot_init();
+  float dist[8];
+  float x = 0, y = 0, theta = 0;
+  float left_steps_prev = 0, right_steps_prev = 0;
+  while (wb_robot_step(TIME_STEP) != -1) {
+    const double *position = wb_supervisor_field_get_sf_vec3f(settings.robot_position);
+    const double *rotation = wb_supervisor_field_get_sf_rotation(settings.robot_rotation);
 
-    para ( int i = 0 ; i < 8 ; i ++) {
-      dist [i] = convert_intensity_to_meters ( wb_distance_sensor_get_value (configurações. ps [i]));
+    for(int i = 0; i < 8; i++) {
+      dist[i] = convert_intensity_to_meters(wb_distance_sensor_get_value(settings.ps[i]));
     }
 
-    salvar_posicao_distancias ( log , posição [ 0 ], posição [ 2 ], rotação [ 3 ], dist);
+    salvar_posicao_distancias(log, position[0], position[2], rotation[3], dist);
 
-    if ( detect_obstacle_ahead (dist))
+    if ( detect_obstacle_ahead(dist) )
     {
-      wb_motor_set_velocity (configurações. left_motor , 0,2 * MAX_SPEED);
-      wb_motor_set_velocity (configurações. right_motor , - 0,2 * MAX_SPEED);
+      wb_motor_set_velocity(settings.left_motor, 0.2 * MAX_SPEED);
+      wb_motor_set_velocity(settings.right_motor, -0.2 * MAX_SPEED);
     }
-    if (! detect_obstacle_ahead (dist))
+    if ( !detect_obstacle_ahead(dist) )
     {
-      wb_motor_set_velocity (configurações. left_motor , 0,5 * MAX_SPEED);
-      wb_motor_set_velocity (configurações. right_motor , 0,5 * MAX_SPEED);
+      wb_motor_set_velocity(settings.left_motor, 0.5 * MAX_SPEED);
+      wb_motor_set_velocity(settings.right_motor, 0.5 * MAX_SPEED);
     }
   }
    
-  fclose ( log );
+  fclose(log);
 
-  wb_robot_cleanup ();
+  wb_robot_cleanup();
 
-  return  0 ;
+  return 0;
 }
